0560bd338f1550dbd70ad8d256e33957eae11335
tamisharutledge <tamisha.rutledge@gmail.com>
2018-10-03 08:46:56 -0500

    Tables has been added.

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,6 +1,6 @@
 USE codeup_test_db;
-
-CREATE TABLE IF NOT EXISTS albums (
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50),
     name VARCHAR(50),
