2a5c7f26a429686e8503f4c802ee431573e90112
tamisharutledge <tamisha.rutledge@gmail.com>
2018-10-04 11:41:30 -0500

    Where exercise, complete.

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,12 +1,22 @@
 USE employees;
 
--- SELECT emp_no, first_name, last_name
+-- SELECT emp_no, first_name, last_name, gender
 -- FROM employees
--- WHERE first_name IN('Irena', 'Vidya', 'Maya');
+-- WHERE gender = 'M'
+-- AND (
+-- first_name IN('Irena', 'Vidya')
+-- OR  first_name = 'Maya'
+-- );
 
 -- SELECT emp_no, first_name, last_name
 -- FROM employees
--- WHERE last_name LIKE 'E%';
+-- WHERE last_name LIKE 'E%'
+-- OR last_name LIKE '%e';
+
+-- SELECT emp_no, first_name, last_name
+-- FROM employees
+-- WHERE last_name LIKE 'E%'
+-- AND last_name LIKE '%e';
 
 -- SELECT emp_no, first_name, last_name, hire_date
 -- FROM employees
@@ -16,6 +26,16 @@ USE employees;
 -- FROM employees
 -- WHERE birth_date LIKE '%-12-25';
 
+-- SELECT emp_no, first_name, last_name, hire_date, birth_date
+-- FROM employees
+-- WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+-- AND birth_date LIKE '%-12-25';
+
 -- SELECT emp_no, first_name, last_name
 -- FROM employees
 -- WHERE last_name LIKE '%q%';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
