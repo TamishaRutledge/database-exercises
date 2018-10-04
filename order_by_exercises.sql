7d8e024d04fe639ccdf50888c1918ebd8c9d1f7f
tamisharutledge <tamisha.rutledge@gmail.com>
2018-10-04 14:21:05 -0500

    Order by exercises.

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -6,12 +6,14 @@ USE employees;
 -- AND (
 -- first_name IN('Irena', 'Vidya')
 -- OR  first_name = 'Maya'
--- );
+-- )
+-- ORDER BY last_name DESC, first_name;
 
 -- SELECT emp_no, first_name, last_name
 -- FROM employees
 -- WHERE last_name LIKE 'E%'
--- OR last_name LIKE '%e';
+-- OR last_name LIKE '%e'
+-- ORDER BY emp_no DESC;
 
 -- SELECT emp_no, first_name, last_name
 -- FROM employees
@@ -29,13 +31,14 @@ USE employees;
 -- SELECT emp_no, first_name, last_name, hire_date, birth_date
 -- FROM employees
 -- WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
--- AND birth_date LIKE '%-12-25';
+-- AND birth_date LIKE '%-12-25'
+-- ORDER BY birth_date ASC , hire_date DESC;
 
 -- SELECT emp_no, first_name, last_name
 -- FROM employees
 -- WHERE last_name LIKE '%q%';
 
-SELECT emp_no, first_name, last_name
-FROM employees
-WHERE last_name LIKE '%q%'
-AND last_name NOT LIKE '%qu%';
+-- SELECT emp_no, first_name, last_name
+-- FROM employees
+-- WHERE last_name LIKE '%q%'
+-- AND last_name NOT LIKE '%qu%';
