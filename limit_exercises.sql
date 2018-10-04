9cf7a02f3ac971573bc6fc41dc5d4e9d12b1e576
tamisharutledge <tamisha.rutledge@gmail.com>
2018-10-04 14:35:11 -0500

    Limit Exercises.

---
+++ limit_exercises.sql
@@ -0,0 +1,12 @@
+USE employees;
+
+-- SELECT DISTINCT last_name FROM employees
+-- ORDER BY last_name DESC
+-- LIMIT 10;
+
+SELECT DISTINCT emp_no, first_name, last_name, hire_date, birth_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY hire_date
+LIMIT 5 OFFSET 49;
