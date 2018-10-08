9fcc6a21bcbd2417b57de2b169ed4473ad2c6792
tamisharutledge <tamisha.rutledge@gmail.com>
2018-10-08 12:27:02 -0500

    Sub Queries

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,9 +1,24 @@
 USE employees;
 
-SELECT first_name, last_name, birth_date
+-- Find all the employees with the same hire date as employee 101010 using a sub-query.
+
+SELECT emp_no, first_name, last_name, hire_date
 FROM employees
+WHERE hire_date IN (
+    SELECT from_date
+    FROM dept_emp
+    WHERE emp_no = 101010
+);
+
+-- Find all the titles held by all employees with the first name Aamod.
+
+SELECT title, emp_no
+FROM titles
 WHERE emp_no IN (
     SELECT emp_no
-    FROM dept_manager
+    FROM employees
+    WHERE first_name = 'Aamod'
 )
-LIMIT 10;
+GROUP BY title, emp_no;
+
+-- Find all the current department managers that are female.
